f532bce39ed8e23ad38a1bb65b8dde9e35760ff9
Bharath1725 <bharathrajkumar25@gmail.com>
2025-04-28 16:10:47 +0530

    Update CollegeManagementSystem.java

--- CollegeManagementSystem.java
+++ CollegeManagementSystem.java
@@ -2,24 +2,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
-class Student {
+class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Student extends Person {
      int studentId;
-     String name;
      String department;
 
     public Student(int studentId, String name, String department) {
+        super(name);
         this.studentId = studentId;
-        this.name = name;
         this.department = department;
     }
 
     public int getStudentId() {
         return studentId;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getDepartment() {
@@ -29,10 +37,10 @@ class Student {
     @Override
     public String toString() {
         return "Student{" +
-               "studentId=" + studentId +
-               ", name='" + name + '\'' +
-               ", department='" + department + '\'' +
-               '}';
+                "studentId=" + studentId +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
     }
 }
 
@@ -56,9 +64,9 @@ class Course {
     @Override
     public String toString() {
         return "Course{" +
-               "courseCode='" + courseCode + '\'' +
-               ", courseName='" + courseName + '\'' +
-               '}';
+                "courseCode='" + courseCode + '\'' +
+                ", courseName='" + courseName + '\'' +
+                '}';
     }
 }
 
@@ -83,16 +91,34 @@ class StudentManagement {
 
 public class CollegeManagementSystem {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         StudentManagement studentManagement = new StudentManagement();
-        studentManagement.addStudent("Bharath", "Information Technology");
-        studentManagement.addStudent("Vijay", "Electrical Engineering");
-        studentManagement.addStudent("Jasiya", "Civil Enginerring");
+        
+        System.out.println("Enter the number of students to add:");
+        int numberOfStudents = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("Enter name of student " + (i + 1) + ":");
+            String name = scanner.nextLine();
+            System.out.println("Enter department of student " + (i + 1) + ":");
+            String department = scanner.nextLine();
+            studentManagement.addStudent(name, department);
+        }
+
         System.out.println("\n--- All Students ---");
         studentManagement.getAllStudents().forEach(System.out::println);
-        Student alice = studentManagement.getStudent(3);
-        if (alice != null) {
+
+        System.out.println("Enter the student ID to retrieve:");
+        int studentId = scanner.nextInt();
+        Student retrievedStudent = studentManagement.getStudent(studentId);
+        if (retrievedStudent != null) {
             System.out.println("\n--- Retrieved Student ---");
-            System.out.println(alice);
+            System.out.println(retrievedStudent);
+        } else {
+            System.out.println("Student with ID " + studentId + " not found.");
         }
+
+        scanner.close();
     }
 }
